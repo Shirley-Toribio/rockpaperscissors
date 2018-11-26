31a442752384030c41422198e9ec2726243fe601
Shirley Toribio <shirleytoribio@gmail.com>
2018-11-26 23:11:20 +0000

    decorate

--- js/script.js
+++ js/script.js
@@ -13,17 +13,48 @@ var winner="";
 // DOCUMENT READY FUNCTION BELOW
 $("#shoot").click(function(){
     var computerChoice="";
-    var userChoice=$("#input").val();
-    $("#userChoice").text(userChoice);
-    var randomNumber=Math.random();
-    if(randomNumber>.7){
-        computerChoice="rock"
+    var userChoice=$("#input").val().toLowerCase().trim();
+    if(userChoice==="rock" ||userChoice==="paper" || userChoice==="scissors" || userChoice==="scissor"){
+        $("#userChoice").text(userChoice);
     }
-    else if(randomNumber<.7 && randomNumber>.29){
-        computerChoice="paper"
+    else if(userChoice===""){
+        $("#userChoice").text("Put something in??");
     }
     else{
-        computerChoice="scissors"
+        $("#userChoice").text("not a valid input douchebag")
+    }
+    var randomNumber=Math.random();
+    if(randomNumber>.7){
+        computerChoice="rock";
+    }
+    else if(randomNumber<.7 && randomNumber>.29){
+        computerChoice="paper";
+    }
+    else{
+        computerChoice="scissors";
+    }
+    if((userChoice==="rock" && computerChoice==="scissor")||(userChoice==="rock" && computerChoice==="scissors")||
+    (userChoice==="paper" && computerChoice==="rock")||(userChoice==="scissors" && computerChoice==="paper")||
+    (userChoice==="scissor" && computerChoice==="paper")){
+        var winner="User";
+        $("#result").text(winner + " wins!");
+        $("#input").val("");
+    }
+    else if((userChoice==="scissor" && computerChoice==="rock")||(userChoice==="scissors" && computerChoice==="rock")||
+    (userChoice==="rock" && computerChoice==="paper")||(userChoice==="paper" && computerChoice==="scissors")||
+    (userChoice==="paper" && computerChoice==="scissor")){
+        var winner="Computer";
+        $("#result").text(winner+"wins");
+        $("#input").val("");
+    }
+    else if(userChoice===""){
+        $("#result").text("Cannot determine");
+    }
+    else{
+        var winner="";
+        $("#result").text("ISSA DRAW");
+        $("#input").val("");
     }
     $("#computerChoice").text(computerChoice);
+    
 })
